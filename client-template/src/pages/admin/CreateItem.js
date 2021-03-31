f738102f3bab92a5343c54ae9a31674b8ae56108
nisha saraswat <nisha0217@gmail.com>
2021-03-31 14:09:31 +0200

    first commit

---
+++ client-template/src/pages/admin/CreateItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CreateItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CreateItem
