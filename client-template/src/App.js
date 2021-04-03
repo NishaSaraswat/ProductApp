431eb85756816848f35e5af676af0fc4e6c860de
nisha saraswat <nisha0217@gmail.com>
2021-04-03 20:11:52 +0200

    UpdateProduct functionality added

--- client-template/src/App.js
+++ client-template/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
 import Items from './pages/Items';
 import Item from './pages/Item';
+import UpdateItem from './pages/admin/UpdateItem';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Switch>
             <Route path="/" exact component={Items} />
             <Route path="/products/:id" component={Item} />
+            <Route path="/update-item/:id" component={UpdateItem}/>    
         </Switch>
       </Router>
     </div>
