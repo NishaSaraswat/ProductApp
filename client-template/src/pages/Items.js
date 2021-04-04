95ac3d60c880e5e4074378e93fe97503e3dc34d6
nisha saraswat <nisha0217@gmail.com>
2021-04-04 23:14:40 +0200

    style added to itemspage

--- client-template/src/pages/Items.js
+++ client-template/src/pages/Items.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
-import Header from '../components/Header';
+
 
 const Items = () => {
     const [items, setItems]=useState([]);
