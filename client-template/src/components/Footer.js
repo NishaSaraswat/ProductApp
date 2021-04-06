3605be3ff18156b2bbbc80360006c31200863f67
Viviann Rydh <rydh.vivian@gmail.com>
2021-04-06 17:29:11 +0200

    fixed items page with styled-component

--- client-template/src/components/Footer.js
+++ client-template/src/components/Footer.js
@@ -4,18 +4,21 @@ import instagramLogo from '../pictures/instagram-icon.jpeg'
 import youtubeLogo from '../pictures/YouTube-icon.png'
 import whatsupLogo from '../pictures/whatsapp-icons.jpeg'
 import twitter from '../pictures/twitter-icon.png'
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+import {FooterWrapper,FooterIcons} from '../itemsStyles'
 
 const Footer = () => {
     return (
         <>
-            <div className="icons-container">
-                <a href="http://www.facebook.com"><img src={facebookLogo} alt="Facebook" className="footer-icon"/></a>
-                <a href="http://www.twitter.com"><img src={twitter} alt="Twitter" className="footer-icon"/></a>
-                <a href="http://www.youtube.com"><img src={youtubeLogo} alt="Youtube"className="footer-icon"/></a>
-                <a href="http://www.instagram.com"><img src={instagramLogo} alt="Instagram"className="footer-icon"/></a>
-                <a href="https://www.whatsapp.com/"><img src={whatsupLogo} alt="Whatsup"className="footer-icon"/></a>
+            <FooterWrapper className="icons-container">
+                <a href="http://www.facebook.com"><FooterIcons src={facebookLogo} alt="Facebook" className="footer-icon"/></a>
+                <a href="http://www.twitter.com"><FooterIcons src={twitter} alt="Twitter" className="footer-icon"/></a>
+                <a href="http://www.youtube.com"><FooterIcons src={youtubeLogo} alt="Youtube"className="footer-icon"/></a>
+                <a href="http://www.instagram.com"><FooterIcons src={instagramLogo} alt="Instagram"className="footer-icon"/></a>
+                <a href="https://www.whatsapp.com/"><FooterIcons src={whatsupLogo} alt="Whatsup"className="footer-icon"/></a>
                 <p>Copy right @ copy right 2021</p>
-            </div>
+            </FooterWrapper>
         </>
     )
 }
