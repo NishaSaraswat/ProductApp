e85f955f7a8e86730feaa22fcbbecdbdca94a373
nisha saraswat <nisha0217@gmail.com>
2021-04-06 14:49:08 +0200

    Changes made in CreateItem page

--- client-template/src/App.js
+++ client-template/src/App.js
@@ -6,6 +6,7 @@ import Item from './pages/Item';
 import UpdateItem from './pages/admin/UpdateItem';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import CreateItem from './pages/admin/CreateItem';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Switch>
             <Route path="/" exact component={Items} />
             <Route path="/products/:id" component={Item} />
+            <Route path="/create-item" component={CreateItem}/>
             <Route path="/update-item/:id" component={UpdateItem}/>    
         </Switch>
         <Footer />
